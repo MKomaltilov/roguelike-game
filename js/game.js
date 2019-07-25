6fea713ecfbc7b40174d66ef6b63e989057f4aa5
Marat Komaltilov <marat.komaltilov@icloud.com>
2019-07-25 20:34:05 +0700

    added simple fight and deleted legacy loading from server (did it before git)

--- js/game.js
+++ js/game.js
@@ -10,6 +10,8 @@ class Location {
 
         this.interaction;
         this.isBlocked = false;
+
+        this.object;
         
     }
 
@@ -78,9 +80,7 @@ class Board {
 
     constructor(size, div) {
         this.size = size;
-        //this.field = [];
         this._fieldElement = div;
-        //this.import();
         
         this.field = [
             [1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
@@ -105,25 +105,8 @@ class Board {
             }
         }
 
-        //random build
-        // for(let x = 0; x < size; x++) {
-        //     let row = [];
-
-        //     for (let y = 0; y < size; y++) {
-        //         let cell;
-        //         (Math.floor(Math.random() * Math.floor(2) > 1)) ? cell = new Wall(x, y) : cell = new Floor(x, y);
-        //         row.push(cell);
-        //     }
-
-        //     this.field.push(row);
-        // }
         this.draw();
 
-    }
-
-    import() {
-        this.field = this.loadField();
-        //this.table = 
     }
 
     export() {
@@ -152,37 +135,16 @@ class Board {
         this._fieldElement.appendChild(fieldElement);
     }
 
-    loadField() {
-        var xmlhttp = new XMLHttpRequest();
-        let response;
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-               if (xmlhttp.status == 200) {
-                   response =  xmlhttp.responseText;
-                   //console.log(response);
-               }
-               else if (xmlhttp.status == 400) {
-                  alert('There was an error 400');
-               }
-               else {
-                   alert('something else other than 200 was returned');
-               }
-            }
-        };
-    
-        xmlhttp.open("GET", "/field", false);
-        xmlhttp.send();
-        return JSON.parse(response);
-    }
 }
 
 class GameObjects {
     constructor(field, player, game) {
         player.X = 9;
-        player.Y = 1
+        player.Y = 0;
         this.objects = [
             player,
             new VerticalEnemy(5, 7),
+            new VerticalEnemy(6, 6),
             new HorizontalEnemy(2, 5)
         ];
         this.field = field;
@@ -197,6 +159,7 @@ class GameObjects {
 
     drawObjects(objects, field, game) {
         for(let object of objects) {
+            field[object.X][object.Y].object = object;
             let objectElement = document.createElement('div');
             objectElement.className = 'game-object game-object-'+ object.type;
             let element = document.getElementById('game-cell-' + object.X + '-' + object.Y);
@@ -206,30 +169,30 @@ class GameObjects {
             if(object instanceof Player && field !== null) {
                 if(field[object.X - 1] !== undefined && field[object.X - 1][object.Y] instanceof Floor) {
                     let element = document.getElementById('game-cell-' + (object.X - 1) + '-' + object.Y);
-                    element.className += ' game-cell-move';
+                    element.className += ' game-cell-point';
                     element.onclick = function() {
-                        game.move(object.X - 1, object.Y);
+                        game.action(object.X - 1, object.Y);
                     }
                 }
                 if(field[object.X + 1] !== undefined && field[object.X + 1][object.Y] instanceof Floor) {
                     let element = document.getElementById('game-cell-' + (object.X + 1) + '-' + object.Y);
-                    element.className += ' game-cell-move';
+                    element.className += ' game-cell-point';
                     element.onclick = function() {
-                        game.move(object.X + 1, object.Y);
+                        game.action(object.X + 1, object.Y);
                     }
                 }
                 if(field[object.X][object.Y + 1] !== undefined && field[object.X][object.Y + 1] instanceof Floor) {
-                    let element = document.getElementById('game-cell-' +object.X + '-' + (object.Y + 1));
-                    element.className += ' game-cell-move';
+                    let element = document.getElementById('game-cell-' + object.X + '-' + (object.Y + 1));
+                    element.className += ' game-cell-point';
                     element.onclick = function() {
-                        game.move(object.X, object.Y + 1);
+                        game.action(object.X, object.Y + 1);
                     }
                 }
                 if(field[object.X][object.Y - 1] !== undefined && field[object.X][object.Y - 1] instanceof Floor) {
-                    let element = document.getElementById('game-cell-' +object.X + '-' + (object.Y - 1));
-                    element.className += ' game-cell-move';
+                    let element = document.getElementById('game-cell-' + object.X + '-' + (object.Y - 1));
+                    element.className += ' game-cell-point';
                     element.onclick = function() {
-                        game.move(object.X, object.Y - 1);
+                        game.action(object.X, object.Y - 1);
                     }
                 }
             }
@@ -246,9 +209,27 @@ class Game {
         this.objects = new GameObjects(this.board.field, this.player, this);
     }
 
-    move(x, y) {
-        this.player.X = x;
-        this.player.Y = y;
+    action(x, y) {
+        if(this.board.field[x][y] instanceof Floor && this.board.field[x][y].object === undefined) {
+            console.log('Move to ' + x + ', ' + y);
+            this.board.field[this.player.X][this.player.Y].object = undefined; 
+            this.player.X = x;
+            this.player.Y = y;
+            this.board.field[this.player.X][this.player.Y].object = this.player;
+            
+        } else if(this.board.field[x][y] instanceof Floor && this.board.field[x][y].object instanceof Enemy) {
+            console.log('Fight started!');
+            this.board.field[x][y].object.hitPoints--;
+            if(this.board.field[x][y].object.hitPoints === 0) {
+                for(let i in this.objects.objects) {
+                    if(this.objects.objects[i].X === x && this.objects.objects[i].Y === y) {
+                        this.objects.objects.splice(i, 1);
+                    }
+                }
+                this.board.field[x][y].object = undefined;
+            }
+        }
+
         this.board.draw();
         this.objects.draw();
     }
