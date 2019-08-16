209a3856b30dcdf4afa1bd83a092f5ef26c68fdb
Marat Komaltilov <marat.komaltilov@icloud.com>
2019-08-16 17:44:46 +0700

    very small fix to clear field after enemy's death

--- src/js/classes/Game.js
+++ src/js/classes/Game.js
@@ -48,6 +48,7 @@ export default class Game {
                 }
                 this.enemiesKilled++;
                 this.board.field[x][y].object = undefined;
+                this.board.field[x][y].isBlocked = false;
             } else {
 
             }
